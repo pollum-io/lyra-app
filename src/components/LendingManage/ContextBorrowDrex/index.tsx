d56174f4c15ae76372fbd688838988a2a1b69fd4
brenno <brennoaraujoqueiroz@gmail.com>
2023-12-01 16:16:52 -0300

    feat: implements contextBorrowDrex component

--- src/components/LendingManage/ContextBorrowDrex/index.tsx
+++ src/components/LendingManage/ContextBorrowDrex/index.tsx
@@ -2,9 +2,10 @@ import { Button } from "@/components/Button";
 import Tabs, { TabContent } from "@/components/Tabs";
 import { EthereumAddress, useBalanceOfDREX } from "@/hooks/useErc20";
 import {
-  getSupplyInterestRate,
-  useGetSuppliedDREX,
-  useSupplyDREX,
+  
+  useBorrowDREX,
+  useGetBorrowedAmount,
+  useGetDepositedTSELIC,
 } from "@/hooks/useRBLLPoolContract";
 import { BigNumberish } from "ethers";
 import Image from "next/image";
@@ -19,6 +20,8 @@ export const ContextBorrowDrex = ({
   dataTotalSupplied: BigNumberish;
   dataTotalBorrowed: BigNumberish;
 }) => {
+  const drexamountsupply = 100 * 1e6;
+
   const {
     data: dataDrexBalance,
     isError: isDrexError,
@@ -26,134 +29,45 @@ export const ContextBorrowDrex = ({
   } = useBalanceOfDREX(address as EthereumAddress);
 
   const {
-    data: dataSuppliedDREX,
-    isError: isErrorSuppliedDREX,
-    isLoading: isLoadingSuppliedDREX,
-  } = useGetSuppliedDREX(address as EthereumAddress);
+    data: dataDepositedTSELIC,
+    isError: isErrorDepositedTSELIC,
+    isLoading: isLoadingDepositedTSELIC,
+  } = useGetDepositedTSELIC(address as EthereumAddress);
+
+  const {
+    data: dataBorrowedAmount,
+    isError: isErrorBorrowedAmount,
+    isLoading: isLoadingBorrowedAmount,
+  } = useGetBorrowedAmount(address as EthereumAddress);
+
+  const {
+    write: writeBorrowDREX,
+    data: dataBorrowDREX,
+    isLoading: isLoadingBorrowDREX,
+    isSuccess: isSuccessBorrowDREX,
+    isError: isErrorBorrowDREX,
+  } = useBorrowDREX(drexamountsupply / 2);
 
   const drexBalance = Number(dataDrexBalance) / 10 ** 6;
-  const drexamountsupply = 100 * 1e6;
-  const [valueDREX, setValueDREX] = useState("");
-  const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(true);
 
-  const {
-    data: dataSupplyInterestRate,
-    isError: isErrorSupplyInterestRate,
-    isLoading: isLoadingSupplyInterestRate,
-  } = getSupplyInterestRate(dataTotalSupplied || 0, dataTotalBorrowed || 0);
-
-  const {
-    write: writeSupplyDREX,
-    data: dataSupplyDREX,
-    isLoading: isLoadingSupplyDREX,
-    isSuccess: isSuccessSupplyDREX,
-    isError: isErrorSupplyDREX,
-  } = useSupplyDREX(drexamountsupply);
-
   useEffect(() => {
-    const loading = [
-      isLoadingDrex,
-      isLoadingSupplyInterestRate,
-      isLoadingSuppliedDREX,
-    ].every((loading) => loading === false);
+    const loading = [isLoadingDrex, isLoadingDepositedTSELIC].every(
+      (loading) => loading === false
+    );
 
     setLoading(!loading);
-  }, [isLoadingDrex, isLoadingSupplyInterestRate, isLoadingSuppliedDREX]);
+  }, [isLoadingDrex, isLoadingDepositedTSELIC]);
 
   return (
     <Tabs>
-      <TabContent title="Depositar">
+      <TabContent title="Emprestar">
         <div className="flex h-[400px] w-full flex-col items-center justify-start gap-6 pt-5">
           <div className="flex w-full items-start justify-start gap-3">
             <div className="flex h-full w-full flex-col items-start justify-start gap-1">
               <div className="inline-flex items-start justify-start gap-6">
                 <div className="text-base font-normal leading-normal text-gray-400">
-                  Balanço: {drexBalance} DREX
-                </div>
-              </div>
-              <div className="border-brandBlue-300 inline-flex w-full items-center justify-between rounded-lg border border-opacity-20 bg-gray-700 px-3">
-                <input
-                  type="number"
-                  className="h-full min-w-[50px] border-none bg-transparent text-base font-normal leading-normal text-white shadow outline-none focus:border-transparent focus:outline-none focus:ring-0"
-                  placeholder={"0"}
-                  value={valueDREX}
-                  onChange={(e) => {
-                    const enteredValue = Number(e.target.value);
-                    if (enteredValue > drexBalance) {
-                      setError("Entered value is greater than current balance");
-                    } else {
-                      setError("");
-                      setValueDREX(e.target.value);
-                    }
-                  }}
-                  style={{
-                    WebkitAppearance: "none",
-                    MozAppearance: "textfield",
-                  }}
-                />
-                <div className="flex w-[100px] items-center justify-center gap-2.5 px-4 py-2">
-                  <button
-                    className="text-brandBlue-300 text-base font-normal leading-normal"
-                    onClick={() => setValueDREX(drexBalance.toString())}
-                  >
-                    max
-                  </button>
-                  <div className="relative h-6 w-6">
-                    <Image
-                      src={"/images/drex.png"}
-                      alt="logo"
-                      layout="fill"
-                      className="rounded-full object-cover"
-                    />
-                  </div>
-                </div>
-              </div>
-              {error && <div className="text-sm text-red-500">{error}</div>}
-            </div>
-          </div>
-
-          <div className="flex h-full w-full flex-col gap-2">
-            <div className=" flex justify-between text-white">
-              <span>APY de Deposito:</span>
-              <span>{Number(dataSupplyInterestRate || 0) / 10e5}%</span>
-            </div>
-            <div className="flex justify-between text-white">
-              <span>Total Depositado:</span>
-              <span>{!isLoading ? `R$ ${dataTotalSupplied}` : "R$ 0"}</span>
-            </div>
-            <div className="flex justify-between text-white">
-              <span>Total Emprestado:</span>
-              <span>{!isLoading ? `R$ ${dataTotalBorrowed}` : "R$ 0"}</span>
-            </div>
-            <div className="flex justify-between text-white">
-              <span>Taxa de Utilização:</span>
-              <span>
-                {!isLoading
-                  ? `${
-                      (Number(dataTotalSupplied) / Number(dataTotalBorrowed)) *
-                      100
-                    }`
-                  : "0"}
-                %
-              </span>
-            </div>
-          </div>
-
-          <Button
-            text="Depositar"
-            onClick={writeSupplyDREX}
-            isLoading={false}
-          />
-        </div>
-      </TabContent>
-      <TabContent title="Sacar">
-        <div className="flex h-[400px] w-full flex-col items-center justify-center gap-6 pt-5">
-          <div className="flex w-full items-start justify-start gap-3">
-            <div className="flex h-full w-full flex-col items-start justify-start gap-4">
-              <div className="inline-flex items-start justify-start gap-6">
-                <div className="text-base font-normal leading-normal text-gray-400">
-                  Balanço: {!isLoading ? `${dataSuppliedDREX}` : "0"} rBRLL
+                  Balanço: {!isLoading ? `${drexBalance}` : "0"} DREX
                 </div>
               </div>
               <div className="border-brandBlue-300 inline-flex w-full items-center justify-between rounded-lg border border-opacity-20 bg-gray-700 px-3">
@@ -176,28 +90,6 @@ export const ContextBorrowDrex = ({
                   </button>
                   <div className="relative h-6 w-6">
                     <Image
-                      src={"/images/brll.png"}
-                      alt="logo"
-                      layout="fill"
-                      className="rounded-full object-cover"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="border-brandBlue-300 inline-flex w-full items-center justify-between rounded-lg border border-opacity-20 bg-gray-700 px-3">
-                <input
-                  type="number"
-                  className="h-full min-w-[50px] border-none bg-transparent text-base font-normal leading-normal text-white shadow outline-none focus:border-transparent focus:outline-none focus:ring-0"
-                  placeholder={"0"}
-                  // value={value} TODO: change props to use control on input
-                  style={{
-                    WebkitAppearance: "none",
-                    MozAppearance: "textfield",
-                  }}
-                />
-                <div className="flex w-[100px] items-center justify-center gap-2.5 px-4 py-2">
-                  <div className="relative h-6 w-6">
-                    <Image
                       src={"/images/drex.png"}
                       alt="logo"
                       layout="fill"
@@ -210,6 +102,14 @@ export const ContextBorrowDrex = ({
           </div>
 
           <div className="flex h-full w-full flex-col gap-2">
+            <div className=" flex justify-between text-white">
+              <span>TSELIC Depositado:</span>
+              <span>{!isLoading ? `${dataDepositedTSELIC}` : "0"} TSELIC</span>
+            </div>
+            <div className="flex justify-between text-white">
+              <span>DREX em Empréstimo:</span>
+              <span>{!isLoading ? `${dataBorrowedAmount}` : "0"} DREX</span>
+            </div>
             <div className=" flex justify-between text-white">
               <span>Liquidez Disponível em DREX:</span>
               <span>
@@ -220,38 +120,22 @@ export const ContextBorrowDrex = ({
                   : "R$ 0"}
               </span>
             </div>
-            <div className="flex justify-between text-white">
-              <span>Total Depositado:</span>
-              <span>{!isLoading ? `R$ ${dataTotalSupplied}` : "R$ 0"}</span>
-            </div>
-            <div className="flex justify-between text-white">
-              <span>Total Emprestado:</span>
-              <span>{!isLoading ? `R$ ${dataTotalBorrowed}` : "R$ 0"}</span>
-            </div>
-            <div className="flex justify-between text-white">
-              <span>Taxa de Utilização:</span>
-              <span>
-                {!isLoading
-                  ? `${
-                      (Number(dataTotalSupplied) / Number(dataTotalBorrowed)) *
-                      100
-                    }`
-                  : "0"}
-                %
-              </span>
-            </div>
           </div>
-
-          <Button text="Sacar" onClick={() => null} isLoading={false} />
+          <Button
+            text="Tomar Emprestimo"
+            onClick={writeBorrowDREX}
+            isLoading={false}
+          />
         </div>
       </TabContent>
-      <TabContent title="Recall">
+
+      <TabContent title="Quitar">
         <div className="flex h-[400px] w-full flex-col items-center justify-start gap-6 pt-5">
           <div className="flex w-full items-start justify-start gap-3">
-            <div className="flex h-full w-full flex-col items-start justify-start gap-4">
+            <div className="flex h-full w-full flex-col items-start justify-start gap-1">
               <div className="inline-flex items-start justify-start gap-6">
                 <div className="text-base font-normal leading-normal text-gray-400">
-                  Balanço: {!isLoading ? `${dataSuppliedDREX}` : "0"} rBRLL
+                  Balanço: {!isLoading ? `${drexBalance}` : "0"} DREX
                 </div>
               </div>
               <div className="border-brandBlue-300 inline-flex w-full items-center justify-between rounded-lg border border-opacity-20 bg-gray-700 px-3">
@@ -274,28 +158,6 @@ export const ContextBorrowDrex = ({
                   </button>
                   <div className="relative h-6 w-6">
                     <Image
-                      src={"/images/brll.png"}
-                      alt="logo"
-                      layout="fill"
-                      className="rounded-full object-cover"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="border-brandBlue-300 inline-flex w-full items-center justify-between rounded-lg border border-opacity-20 bg-gray-700 px-3">
-                <input
-                  type="number"
-                  className="h-full min-w-[50px] border-none bg-transparent text-base font-normal leading-normal text-white shadow outline-none focus:border-transparent focus:outline-none focus:ring-0"
-                  placeholder={"0"}
-                  // value={value} TODO: change props to use control on input
-                  style={{
-                    WebkitAppearance: "none",
-                    MozAppearance: "textfield",
-                  }}
-                />
-                <div className="flex w-[100px] items-center justify-center gap-2.5 px-4 py-2">
-                  <div className="relative h-6 w-6">
-                    <Image
                       src={"/images/drex.png"}
                       alt="logo"
                       layout="fill"
@@ -307,12 +169,18 @@ export const ContextBorrowDrex = ({
             </div>
           </div>
 
-          <div className="flex h-full w-full  text-white">
-            O recall instantâneo facilita a troca de garantias em TSELIC por
-            DREX através da Uniswap.
+          <div className="flex h-full w-full flex-col gap-2">
+            <div className="flex justify-between text-white">
+              <span>Passivo em DREX:</span>
+              <span>{!isLoading ? `${dataBorrowedAmount}` : "0"} DREX</span>
+            </div>
           </div>
 
-          <Button text="Recall" onClick={() => null} isLoading={false} />
+          <Button
+            text="Quitar Emprestimo"
+            onClick={() => null}
+            isLoading={false}
+          />
         </div>
       </TabContent>
     </Tabs>
