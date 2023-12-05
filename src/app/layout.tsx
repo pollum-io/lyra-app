815b0aeef14e606be0f9eb3e00891be3d950576b
brenno <brennoaraujoqueiroz@gmail.com>
2023-12-05 18:05:07 -0300

    chore: fix head siteconfig

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ import { ConnectWallet } from "@/components/ConnectWallet";
 import { LendingManage } from "@/components/LendingManage";
 import ToastNotification from "@/components/Toast";
 import { useToastStore } from "@/stores/toast";
+import { metadata } from "@/constant/config";
+import Head from "next/head";
+
 const mulish = Mulish({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -25,6 +28,28 @@ export default function RootLayout({
 
   return (
     <html suppressHydrationWarning lang="en">
+      <head>
+        <title>{metadata.title.default}</title>
+        <meta name="description" content={metadata.description} />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={metadata.icons.apple}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={metadata.icons.icon}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={metadata.icons.icon}
+        />
+        <link rel="manifest" href={metadata.manifest} />
+      </head>
       <body className={`${mulish.className} m-0 h-screen p-0`}>
         <Providers>
           <div className="bg-primary radial-gradient-bg flex min-h-screen flex-col">
--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  title: 'Lyra loans',
+  title: 'Lyra Loans',
   description:
     'Lyra Loans é um protocolo de empréstimos descentralizado desenvolvido para o HACKATHON: Web3 – Tokenização do Tesouro Nacional.',
 };
